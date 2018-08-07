1ce936b656e57dd373eca8edc02b1f786ea84ea1
islishude <lishude@aliyun.com>
2018-08-07 19:03:10 +0800

    test: use assert instead of console.assert

--- test/erc20.ts
+++ test/erc20.ts
@@ -1,4 +1,5 @@
-import { assert, log } from "console";
+import { equal } from "assert";
+import { log } from "console";
 import { Ethereum } from "../src/index";
 /* spell-checker: disable */
 
@@ -8,10 +9,10 @@ const TRX = "0xf230b790e05390fc8295f4d3f60332c93bed42e2";
 Client.ERC20TokenInfo(TRX)
   .then(({ decimals, name, totalSupply, symbol }) => {
     log(decimals, name, symbol, totalSupply);
-    assert(decimals === 6);
-    assert(name === "Tronix");
-    assert(symbol === "TRX");
-    assert(totalSupply === "100000000000");
+    equal(decimals, 6);
+    equal(name, "Tronix");
+    equal(symbol, "TRX");
+    equal(totalSupply, "100000000000");
   })
   .catch(err => log(err.message));
 
@@ -19,10 +20,10 @@ const HPB = "0x38c6a68304cdefb9bec48bbfaaba5c5b47818bb2";
 Client.ERC20TokenInfo(HPB)
   .then(({ decimals, name, totalSupply, symbol }) => {
     log(decimals, name, symbol, totalSupply);
-    assert(decimals === 18);
-    assert(name === "HPBCoin");
-    assert(symbol === "HPB");
-    assert(totalSupply === "100000000");
+    equal(decimals, 18);
+    equal(name, "HPBCoin");
+    equal(symbol, "HPB");
+    equal(totalSupply, "100000000");
   })
   .catch(err => log(err.message));
 
@@ -30,9 +31,9 @@ const EGCC = "0xaf8a215e81faea7c180ce22b72483525121813bd";
 Client.ERC20TokenInfo(EGCC)
   .then(({ decimals, name, totalSupply, symbol }) => {
     log(decimals, name, symbol, totalSupply);
-    assert(decimals === 18);
-    assert(name === "EngineToken");
-    assert(symbol === "EGCC");
-    assert(totalSupply === "10000000000");
+    equal(decimals, 18);
+    equal(name, "EngineToken");
+    equal(symbol, "EGCC");
+    equal(totalSupply, "10000000000");
   })
   .catch(err => log(err.message));
