3874991982c24fc72ca6a9a30c1ec5b7133e35d9
islishude <lishude@aliyun.com>
2018-09-11 21:36:29 +0800

    feat: add example

---
+++ example/bitcoin.ts
@@ -0,0 +1,83 @@
+import { log } from "console";
+import {
+  BitcoinClient,
+  BitcoinMethods,
+  IBtcTxInfo,
+  IRpcConfig,
+  IRpcRequest
+} from "wallet-rpc";
+
+const DefaultBtcRpcConf: IRpcConfig = {
+  ip: "http://127.0.0.1",
+  pass: "",
+  port: "8332",
+  user: ""
+};
+
+const BtcClient = new BitcoinClient(DefaultBtcRpcConf);
+
+// Simple Example
+export const example0 = async () => {
+  const txid =
+    "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098";
+  const { error, result, id } = await BtcClient.getTxInfo(txid);
+  if (error !== undefined) {
+    // ...
+  }
+  log(id);
+  return result;
+};
+
+// BulkCall example
+export const example1 = async () => {
+  const txidList: string[] = [
+    "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098",
+    "ce3ab453f96020a32ca382d07967231fa463cf1f365ce4bdc52764faf20371bf"
+  ];
+
+  const reqData: IRpcRequest[] = txidList.map(txid => {
+    const tmp: IRpcRequest = {
+      id: txid,
+      jsonrpc: "2.0",
+      method: BitcoinMethods.tx.detail,
+      params: [txid]
+    };
+    return tmp;
+  });
+
+  const res = await BtcClient.BulkRpcExec<IBtcTxInfo>(reqData);
+
+  for (const { result, error } of res) {
+    if (error !== undefined) {
+      // ...
+    }
+    log("%O", result);
+  }
+};
+
+// Another BulkCall example(NOT RECOMMEND)
+export const example3 = async () => {
+  const txidList: string[] = [
+    "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098",
+    "ce3ab453f96020a32ca382d07967231fa463cf1f365ce4bdc52764faf20371bf"
+  ];
+
+  for (const txid of txidList) {
+    const tmp: IRpcRequest = {
+      id: txid,
+      jsonrpc: "2.0",
+      method: BitcoinMethods.tx.detail,
+      params: [txid]
+    };
+    BtcClient.BulkAdd(tmp);
+  }
+
+  const res = await BtcClient.BulkRpcCall<IBtcTxInfo>();
+
+  for (const { result, error } of res) {
+    if (error !== undefined) {
+      // ...
+    }
+    log("%O", result);
+  }
+};
---
+++ example/ethereum.ts
@@ -0,0 +1,57 @@
+import { log } from "console";
+import { EthereumClient, IRpcConfig } from "wallet-rpc";
+
+const DefaultEthRpcConf: IRpcConfig = {
+  ip: "http://127.0.0.1",
+  pass: "",
+  port: "8545",
+  user: ""
+};
+
+const EthClient = new EthereumClient(DefaultEthRpcConf);
+
+export const example0 = async () => {
+  const txid =
+    "0xc4f1806717dd22e89d158ae1466f9ebc0124d2169843a7bcc65975c8a8327854";
+
+  // In production you should use Promise.all
+  const { result: txInfo } = await EthClient.getTxByHash(txid);
+  const { result: txReceipt } = await EthClient.getTxReceipt(txid);
+  // If you connect the parity
+  const { result: txTrance } = await EthClient.traceTxByParity(txid);
+
+  log(txInfo, txReceipt, txTrance);
+};
+
+export const example1 = async () => {
+  const address = "0x0";
+
+  const { result: latest } = await EthClient.getBalance(address, "latest");
+  const { result: pending } = await EthClient.getBalance(address, "pending");
+  log(latest, pending);
+};
+
+export const example2 = async () => {
+  const address = "0x0";
+  const token = "0x1";
+  const latest = await EthClient.ERC20Balance(token, address, false);
+  const pending = await EthClient.ERC20Balance(token, address, true);
+  log(latest, pending);
+
+  const TRX = "0xf230b790e05390fc8295f4d3f60332c93bed42e2";
+  const {
+    decimals,
+    name,
+    totalSupply,
+    symbol
+  } = await EthClient.ERC20TokenInfo(TRX);
+  log(decimals, name, totalSupply, symbol);
+};
+
+export const example3 = async () => {
+  const address = "0x0";
+  const nonce = await EthClient.getAddrNonce(address);
+  // If you connect the parity
+  const pendingNonce = await EthClient.getAddrNextNonce(address);
+  log(nonce, pendingNonce);
+};
