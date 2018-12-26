7dc71db58e13cac3d4da5151a33361659b0e80b6
islishude <lishude@aliyun.com>
2018-12-26 17:20:48 +0800

    test: use Object.is

--- test/testing.ts
+++ test/testing.ts
@@ -1,15 +1,12 @@
-import { strictEqual } from "assert";
-
 export function Testing(
   testFunc: any,
   testData: Array<{ expect: any; param: any; name: string }>
 ) {
   for (const { param, expect, name } of testData) {
     const actual = testFunc(param);
-    strictEqual(
-      actual,
-      expect,
-      `${name} test failed want ${expect} but got ${actual}`
-    );
+
+    if (!Object.is(actual, expect)) {
+      throw new Error(`${name} test failed want ${expect} but got ${actual}`);
+    }
   }
 }
